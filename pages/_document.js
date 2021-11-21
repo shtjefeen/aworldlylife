f4c659774759d980acf90f6f9d6e15d68c66b0fa
Shtjefen Hilaj <shtjefeen@gmail.com>
2021-11-21 11:42:57 +0100

    Facebook pixel

--- pages/_document.js
+++ pages/_document.js
@@ -66,7 +66,10 @@ class MyDocument extends Document {
             content="/favicon/browserconfig.xml"
           />
           <meta name="theme-color" content="#ffffff" />
-          <meta name="facebook-domain-verification" content="74pdlo2omdcuxujd4izzxbmmruxt5n" />
+          <meta
+            name="facebook-domain-verification"
+            content="74pdlo2omdcuxujd4izzxbmmruxt5n"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
@@ -96,19 +99,46 @@ class MyDocument extends Document {
           />
           <Script src="https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js" />
 
-          {/* Google tag Manager */}
-          <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WKGXQ23')`}}>
-          </script>
+          <Script id="google-tag-manager">
+            {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WKGXQ23')
+          `}
+          </Script>
+          <Script id="facebook-pixel">
+            {`
+           !function(f,b,e,v,n,t,s)
+           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+           n.queue=[];t=b.createElement(e);t.async=!0;
+           t.src=v;s=b.getElementsByTagName(e)[0];
+           s.parentNode.insertBefore(t,s)}(window, document,'script',
+           'https://connect.facebook.net/en_US/fbevents.js');
+           fbq('init', '1577678739252182');
+           fbq('track', 'PageView');
+          `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1577678739252182&ev=PageView&noscript=1"
+            />
+          </noscript>
         </Head>
 
         <body>
-          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKGXQ23"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}>
-          </noscript>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKGXQ23"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
